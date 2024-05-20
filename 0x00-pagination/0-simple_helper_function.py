155d310904bed0261965a1287389c3cf1b7f443d
RedarsHubi <redamasters@gmail.com>
2024-05-20 13:27:13 +0100

    Red checker?

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
-from typing import Tuple
 """
 returns a tuple of size two
 """
+from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """takes two integer arguments page and page_size returns tuple"""
-    start_index: int = (page - 1) * page_size
-    return (start_index, start_index + page_size)
+    return ((page - 1) * page_size, page_size * page)
