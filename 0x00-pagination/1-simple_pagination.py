cc9c0a7a77e83489b8c635fcf62ad79c7b2ae790
RedarsHubi <redamasters@gmail.com>
2024-05-20 13:57:11 +0100

    Baby names

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+returns a tuple of size two
+"""
+from typing import Tuple, List
+import csv
+import math
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """takes two integer arguments page and page_size returns tuple"""
+    return ((page - 1) * page_size, page_size * page)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """finds indexes to paginate the dataset and return the page"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        start, end = index_range(page, page_size)
+        dataset = self.dataset()
+
+        if start >= len(dataset):
+            return []
+        return dataset[start:end]
