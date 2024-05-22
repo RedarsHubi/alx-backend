3f6971c8a4a21cb243250a91f2fc34121e897f3e
RedarsHubi <redamasters@gmail.com>
2024-05-22 18:46:11 +0100

    FIFO Caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,25 +1,31 @@
 #!/usr/bin/python3
-""" BasicCache module
+""" FIFOCache module
 """
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class BasicCache(BaseCaching):
+class FIFOCache(BaseCaching):
     """ caching system
     """
     def __init__(self):
         """ Initiliaze
         """
         super().__init__()
+        self.__queue = []
 
     def put(self, key, item):
-        """ Add an item in the cache
+        """ Adds an item in the cache
         """
         if not key or not item:
             return
+        if key not in self.__queue:
+            self.__queue.append(key)
+        if len(self.__queue) > self.MAX_ITEMS:
+            self.cache_data.pop(self.__queue[0])
+            print('DISCARD: {}'.format(self.__queue.pop(0)))
         self.cache_data.update({key: item})
 
     def get(self, key):
-        """ Get an item by key
+        """ Gets an item by key
         """
         return self.cache_data.get(key)
