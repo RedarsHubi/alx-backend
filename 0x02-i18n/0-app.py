11a0c7403d98e5065134562992807062aca2cb68
RedarsHubi <redamasters@gmail.com>
2024-05-28 17:21:39 +0100

    Home

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python3
-""" Basic Flask app
-"""
+"""basic flask app"""
 
 from flask import Flask, render_template
-from flask_babel import Babel
-
 app = Flask(__name__)
-babel = Babel(app)
 
 
 @app.route('/', strict_slashes=False)
-def home():
-    """Home"""
+def index():
+    """ home """
     return render_template('0-index.html')
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
