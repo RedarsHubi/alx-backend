a74e9a1590707b89b0eef36852c2b6a4ddbd73fd
RedarsHubi <redamasters@gmail.com>
2024-05-28 18:05:21 +0100

    Resto

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -25,12 +25,23 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Gets locale from request object
+    """
+    locale = request.args.get('locale', '').strip()
+    if locale and locale in Config.LANGUAGES:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """
     Renders a basic html template
     """
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
